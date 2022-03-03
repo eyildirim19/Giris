51f4a1221e120754c0e6f5f7b51431b37d766d37
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-03-03 20:07:46 +0300

    ödev çözümleri ve yeni ödevler..

--- Metot7/Program.cs
+++ Metot7/Program.cs
@@ -34,6 +34,8 @@
 
         // nums dizisi içerisindeki elamanları benzersiz yeni bir diziye eklenecek, adetleri yeni bir diziyi eklenecek...
 
+
+
         int[] nums = { 1, 2, 2, 3, 3, 3, 4, 5, 8, 8, 8, 8 };
         // {1,2,3,4,5,8}
         // {1,2,3,1,1,4}
@@ -48,7 +50,8 @@
             if (Array.IndexOf(uniqueNums, item) > -1) // sıradaki eleman unique dizimizde var mı?.Eğer varsa -1 büyük dön;
                 continue; // sonraki eleman geç...
 
-            int adet = ElemanAdet(nums, item); // item nums dizisinde kaç tane? bul ve numscount'in i. ci ata...
+            // int adet = ElemanAdet(nums, item); // item nums dizisinde kaç tane? bul ve numscount'in i. ci ata...
+            int adet = ElemanAdetV2(nums, item);
             Array.Resize(ref uniqueNums, uniqueNums.Length + 1); // dizide yeni alan aç
             Array.Resize(ref numsCount, numsCount.Length + 1); // dizide yeni alana aç
 
@@ -60,12 +63,90 @@
         {
             Console.WriteLine($"{uniqueNums[i]} elemanından {numsCount[i]} adet var...");
         }
+
+
+        int[] sirasizlar = { 100, 5, 50, 2, 10, 99, 8, 4, 2 };
+        int[] siralanmis = Sirala(sirasizlar); // sırasızlar dizinin siralanmış halini siralnamış dizisine atar...
+        foreach (var item in siralanmis)
+        {
+            Console.WriteLine(item);
+        }
+
+        //
+        char[] harfler = { 'A', 'B', 'C', 'd', 'E', 'z', 'P', 'U', 'K', 'n' };
+        char[] bHarfler = new char[0];
+        foreach (var item in harfler)
+        {
+            if (!IsBuyuk(item))
+                continue;
+
+            Array.Resize(ref bHarfler, bHarfler.Length + 1);
+            bHarfler[bHarfler.Length - 1] = item;
+        }
+
+        char[] bHarfler2 = new char[0];
+        foreach (var item in harfler)
+        {
+            char temp = char.ToLower(item); // küçük harfe çevir... // d
+
+            if (item == temp) // d = d
+                continue;
+
+            Array.Resize(ref bHarfler2, bHarfler2.Length + 1);
+            bHarfler2[bHarfler2.Length - 1] = item;
+        }
+
+    }
+    // A (65) B(66) C(67).......Z(90)
+    static bool IsBuyuk(char c)
+    {
+        // (int)c => char'in sayısal karşılığını alalım..
+        int k = (int)c;
+        if (k >= 65 && k <= 90)
+            return true;
+        else
+            return false;
     }
 
     // todo: char[] harfler ={'A','B','C','d','E',z,P,U,K,n} harfler dizinindeki büyük harfleri yeni bir diziye ekleyiniz...beklediğimiz dizi yeniHarfler = {'A','B','C','E','P','U','K'} => ipucu büyük harfleri bulurken ascii code karşılığını kullanabilirsiniz....
     // ascii table araştırınız...
     // PERŞEMBE GÜNÜ OOP KONUSUNA BAŞLIYORUZ.. (ARAŞTINIZ...)
 
+    static int[] Sirala(int[] arr)
+    {
+        int[] sirali = new int[0];
+        // todo: karışık elemanlı arr dizisini sıralı bir şekilde sirali dizisine ekleyerek dönünüz.. Bu işlemi yaparken bubble sort algoritmasını kulllanınız...
+        // izleyiniz..https://www.youtube.com/watch?v=semGJAJ7i74&list=PLOmdoKois7_FK-ySGwHBkltzB11snW7KQ&index=2
+
+        return sirali;
+    }
+
+    static int BinarySearch(int[] arr, int eleman)
+    {
+        int counter = 0;
+        //todo : Binary Search algoritması ile aranan elemanın adetini bulunur...
+        // 8
+
+        // A=  1,2,2,3,3,3
+        // B = 4,5,8,8,8,8
+
+        // 8
+        // 1,2,2,3,3,3,4,5,8,8,8,8
+        return counter;
+    }
+
+    static int ElemanAdetV2(int[] arr, int eleman)
+    {
+        int counter = 0;
+        int index = Array.IndexOf(arr, eleman);  // eleman varsa indexi'ini döner, yoksa -1 döner..
+        while (index > -1)
+        {
+            counter++;
+            // startIndex parametresi ile dizinin belirlenen indexinde arama yapılır...
+            index = Array.IndexOf(arr, eleman, index + 1); // bulnan indexden sonra  
+        }
+        return counter;
+    }
     static int ElemanAdet(int[] arr, int eleman)// aranaca dizi, aranacak elemean...
     {
         // todo: Bu bölümü Array sınıfının indexOf metodunu kullarak yapınız.. 01.03.2022
@@ -73,9 +154,9 @@
         for (int i = 0; i < arr.Length; i++)
             if (eleman == arr[i])  // eleman dizinin i. eşit mi  
                 count++; // adeti 1 arttır
-
         return count;
     }
+
     static int Karsilastir(int sayi1, int sayi2)
     {
         if (sayi1 > sayi2)
